d65210afe1c57ce83c1c2f7738695ceaef74dce8
Victor <victor.gavioli2004@gmail.com>
2025-01-27 11:35:30 +0100

    finished checking

--- main.py
+++ main.py
@@ -12,50 +12,65 @@ def convertir(x, y):
     col = x // SIZE
     return int(row), int(col)
 
-# Vérifie si une des 11 cases adjacentes à la case cliqué n'est pas vide 
-def adjacent(row,col):
-    for i in range(-1,2):
-        for j in range(-1,2):
-            if (i == 0) and (j == 0):
-                continue
-            if (0 <= row+i < 8) and (0 <= col+j < 8): 
-                if(board[row+i][col+j] != 0):
-                    return False
-    return True
-
 # Vérifie si on peut retourner les pièces ou non
 def can_flip(row,col):
     for i in range(-1,2):
         for j in range(-1,2):
+
             if(i == 0) and (j == 0):
                 continue
-        r, c = row + i, col + j
-        if((0 <= r < 8) and (0 <= c < 8)) and (board[r][c] != player_turn) and (board[r][c] != 0):
-            while (0 <= r < 8) and (0 <= c < 8):
-                r+=i
-                j+=i
-                if not (0 <= r < 8 and 0 <= c < 8):
-                    break
-                if board[r][c] == 0:
-                    break
-                if board[r][c] == player_turn:
-                    return True
+            r, c = row + i, col + j
+            if((0 <= r < 8) and (0 <= c < 8)):
+                while((0 <= r < 8 and 0 <= c < 8) and (board[r][c] != 0 and board[r][c] != player_turn)):
+                    r+=i
+                    c+=j
+                    if not (0 <= r < 8 and 0 <= c < 8):
+                        break
+                    if board[r][c] == 0:
+                        break
+                    if board[r][c] == player_turn:
+                        return True
     return False
+
+def flip(row,col):
+    for i in range(-1,2):
+        for j in range(-1,2):
+            if(i == 0) and (j == 0):
+                continue
+            r, c = row + i, col + j
+            if((0 <= r < 8) and (0 <= c < 8)):
+                while((0 <= r < 8 and 0 <= c < 8) and (board[r][c] != 0 and board[r][c] != player_turn)):
+                    if(player_turn == 1):
+                        board[r][c] = 1
+                        canvas.create_oval(c * SIZE + 5, r * SIZE + 5,(c + 1) * SIZE - 5, (r + 1) * SIZE - 5, fill="black")
+                    else:
+                        board[r][c] = 2
+                        canvas.create_oval(c * SIZE + 5, r * SIZE + 5,(c + 1) * SIZE - 5, (r + 1) * SIZE - 5, fill="white")
+                    r+=i
+                    c+=j
+                    if not (0 <= r < 8 and 0 <= c < 8):
+                        break
+                    if board[r][c] == 0:
+                        break
+                    if board[r][c] == player_turn:
+                        break
 
 #Quand on clique sur la case, on vérifie si on peut bien poser la tuile et on pose une tuile de la couleur du joueur correspondant.
 def on_click(event):
     global board, player_turn
     row, col = convertir(event.x, event.y)
-    if(board[row][col] == 0 and (adjacent(row,col))) and (can_flip(row,col)):
+    if(board[row][col] == 0 and not(can_flip(row,col))):
         return
     if board[row][col] == 0:
         if player_turn == 1:
             board[row][col] = 1
             canvas.create_oval(col * SIZE + 5, row * SIZE + 5,(col + 1) * SIZE - 5, (row + 1) * SIZE - 5, fill="black")
+            flip(row,col)
             player_turn = 2
         else:
             board[row][col] = 2
             canvas.create_oval(col * SIZE + 5, row * SIZE + 5,(col + 1) * SIZE - 5, (row + 1) * SIZE - 5, fill="white")
+            flip(row,col)
             player_turn = 1
 
 # Les 4 tuiles du milieu de plateau
